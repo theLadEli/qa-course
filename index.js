a06e49fc48ebaecbe41e0599eb18c31077386ce2
Eli <109231330+theLadEli@users.noreply.github.com>
2023-03-28 13:45:58 +0100

    Saved the name to a variable thats displayed on the alert

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+var yourName;
+
+function storeName() {
+    yourName = document.getElementById("name").value;
+    console.log(yourName);
+}
+
 function formSubmission(event) {
-    // prevent default form submission behavior
     event.preventDefault();
-  
-    alert("Thanks for your submission!");
+
+    alert("Thanks for your submission " + yourName + ", we'll get back to you as soon as we can!");
     // reset the form
     document.getElementById("myForm").reset();
-  }
+}
